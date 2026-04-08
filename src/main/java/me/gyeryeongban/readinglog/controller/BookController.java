de42f9e2c2e4fb28d48f0e9951f7fb09c1a0f3ff
gyeryeongban <gyeryeong.ban@gmail.com>
2026-04-08 13:10:42 +0900

    feat: implement BookController for REST API endpoints

---
+++ src/main/java/me/gyeryeongban/readinglog/controller/BookController.java
@@ -0,0 +1,26 @@
+package me.gyeryeongban.readinglog.controller;
+
+import me.gyeryeongban.readinglog.domain.Book;
+import me.gyeryeongban.readinglog.service.BookService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.findAll();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+}
