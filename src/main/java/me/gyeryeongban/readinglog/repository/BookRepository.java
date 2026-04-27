f446a06b403a9244a933683e6fa121bda313910c
gyeryeongban <gyeryeong.ban@gmail.com>
2026-04-27 23:35:13 +0900

    feat: add findByStatus query method to BookRepository

--- src/main/java/me/gyeryeongban/readinglog/repository/BookRepository.java
+++ src/main/java/me/gyeryeongban/readinglog/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package me.gyeryeongban.readinglog.repository;
 
 import me.gyeryeongban.readinglog.domain.Book;
+import me.gyeryeongban.readinglog.domain.BookStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    List<Book> findByStatus(BookStatus status);
 }
