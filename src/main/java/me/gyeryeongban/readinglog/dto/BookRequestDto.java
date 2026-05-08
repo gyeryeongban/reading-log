dadbef59f1650c8b6f5c7a0c0407552349051743
gyeryeongban <gyeryeong.ban@gmail.com>
2026-05-08 17:19:52 +0900

    feat: add BookRequestDto and BookResponseDto

---
+++ src/main/java/me/gyeryeongban/readinglog/dto/BookRequestDto.java
@@ -0,0 +1,4 @@
+package me.gyeryeongban.readinglog.dto;
+
+public record BookRequestDto(String title, String author, Integer price) {
+}
---
+++ src/main/java/me/gyeryeongban/readinglog/dto/BookResponseDto.java
@@ -0,0 +1,4 @@
+package me.gyeryeongban.readinglog.dto;
+
+public record BookResponseDto(Long id, String title, String author) {
+}
